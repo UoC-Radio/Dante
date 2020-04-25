bc0da7ad40a75922eb3b3f802811b1a0e764139a
kadilier <kadilier@csd.uoc.gr>
2020-04-25 19:54:57 +0300

    check schedule for overlap of show_weekday before insert

--- api/controllers/show_weekdays_controller.go
+++ api/controllers/show_weekdays_controller.go
@@ -13,12 +13,13 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strconv"
+	"strings"
 	"time"
 )
 
 /*
 Same POST:
-	curl --header "Content-Type: application/json" --request POST --data '{"id_shows":420,"duration":"2h", "start_time":"2081-02-01T12:51:00+00:02"}' http://localhost:8080/weekdays/5/shows
+	curl --header "Content-Type: application/json" --request POST --data '{"id_shows":420,"duration":"2h", "start_time":"0000-01-01T12:51:00+00:02"}' http://localhost:8080/weekdays/5/shows
 */
 func (server *Server) AddShowOnWeekday(w http.ResponseWriter, r *http.Request) {
 
@@ -26,7 +27,7 @@ func (server *Server) AddShowOnWeekday(w http.ResponseWriter, r *http.Request) {
 	weekDayId, err := strconv.Atoi(vars["id"])
 
 	// check that weekday exist
-	exists, err := models.ShowWeekdayExists(context.Background(), server.DB, weekDayId)
+	exists, err := models.WeekDayExists(context.Background(), server.DB, weekDayId)
 	if err != nil {
 		responses.ERROR(w, http.StatusBadRequest, err)
 		return
@@ -68,6 +69,14 @@ func (server *Server) AddShowOnWeekday(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// schedule check
+	showsOnWeekday, _ := models.ShowWeekdays(qm.Where("id_week_days=?", showWeekday.IDWeekDays)).All(context.Background(), server.DB)
+	err = checkOverlap(showWeekday, showsOnWeekday)
+	if err != nil {
+		responses.ERROR(w, http.StatusBadRequest, err)
+		return
+	}
+
 	err = showWeekday.Insert(context.Background(), server.DB, boil.Infer())
 	if err != nil {
 		responses.ERROR(w, http.StatusInternalServerError, err)
@@ -99,5 +108,35 @@ func (server *Server) RemoveShowFromWeekday(w http.ResponseWriter, r *http.Reque
 	}
 
 	responses.JSON(w, http.StatusNoContent, "")
+
+}
+
+/* helper function */
+
+// checks whether a showWeekDay overlaps in schedule (returns error)
+func checkOverlap(showWeekday models.ShowWeekday, showsOnWeekday models.ShowWeekdaySlice) error {
+
+	startTime := showWeekday.StartTime
+	duration, _ := time.ParseDuration(showWeekday.Duration)
+	endTime := startTime.Add(duration)
+
+	// iterate through all other (scheduled) show_weekdays
+	for _, sw := range showsOnWeekday {
+
+		swStart := sw.StartTime
+		durationSplit := strings.Split(sw.Duration, ":") //convert sql interval (e.g. "02:00:00") -> golang time.Duration ("2h0m0s")
+		swDuration, err := time.ParseDuration(durationSplit[0] + "h" + durationSplit[1] + "m" + durationSplit[2] + "s")
+		if err != nil {
+			return err
+		}
+		swEnd := swStart.Add(swDuration)
+
+		if startTime.Before(swEnd) && swStart.Before(endTime) {
+			return errors.New("overlaps with show: " + strconv.Itoa(sw.IDShows.Int))
+		}
+
+	}
+
+	return nil
 
 }
