4b1f2ab748ee93a21aa2bf01106a69235be4f05f
ggalan <ggalan87@gmail.com>
2020-04-29 19:57:46 +0300

    converted var name to camelCase

--- api/controllers/messages_controller.go
+++ api/controllers/messages_controller.go
@@ -67,14 +67,14 @@ func (server *Server) GetMessages(w http.ResponseWriter, r *http.Request) {
 	var err error
 
 	if found {
-		page_idx, err := strconv.Atoi(page)
+		pageIdx, err := strconv.Atoi(page)
 
 		if err != nil {
 			responses.ERROR(w, http.StatusBadRequest, err)
 			return
 		}
 
-		messages, err = models.ShowMessages(qm.Where("id_shows=?", show_id), qm.Limit(20), qm.Offset(page_idx*20)).All(context.Background(), server.DB)
+		messages, err = models.ShowMessages(qm.Where("id_shows=?", show_id), qm.Limit(20), qm.Offset(pageIdx*20)).All(context.Background(), server.DB)
 	} else {
 		messages, err = models.ShowMessages(qm.Where("id_shows=?", show_id)).All(context.Background(), server.DB)
 	}
