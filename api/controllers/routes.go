6b62f3740f3b6f1784f0b7f67f790ca350416a20
ggalan <ggalan87@gmail.com>
2020-04-29 22:28:21 +0300

    added some regex verification to routes

--- api/controllers/routes.go
+++ api/controllers/routes.go
@@ -4,36 +4,35 @@ import "Dante/api/middlewares"
 
 func (s *Server) initializeRoutes() {
 
+	idRegEx := "[0-9]+"
+
 	// Home Route
 	s.Router.HandleFunc("/", middlewares.SetMiddlewareJSON(s.Home)).Methods("GET")
 
 	// Users routes
 	s.Router.HandleFunc("/members/{username}", middlewares.SetMiddlewareIPFilter(s.GetMember)).Methods("GET")
 
-	s.Router.HandleFunc("/weekdays/{id}/shows", middlewares.SetMiddlewareJSON(s.GetShowsWeekDay)).Methods("GET")
-	s.Router.HandleFunc("/weekdays/{id}/shows", middlewares.SetMiddlewareJSON(s.AddShowOnWeekday)).Methods("POST")
-	s.Router.HandleFunc("/weekdays/{id}/shows/{show_id}", middlewares.SetMiddlewareJSON(s.RemoveShowFromWeekday)).Methods("DELETE")
+	s.Router.HandleFunc("/weekdays/{id:[1-7]}/shows", middlewares.SetMiddlewareJSON(s.GetShowsWeekDay)).Methods("GET")
+	s.Router.HandleFunc("/weekdays/{id:[1-7]}/shows", middlewares.SetMiddlewareJSON(s.AddShowOnWeekday)).Methods("POST")
+	s.Router.HandleFunc("/weekdays/{id:[1-7]}/shows/{show_id:"+idRegEx+"}", middlewares.SetMiddlewareJSON(s.RemoveShowFromWeekday)).Methods("DELETE")
 
 	s.Router.HandleFunc("/shows", middlewares.SetMiddlewareIPFilter(s.GetShows)).Methods("GET")
 	s.Router.HandleFunc("/shows", middlewares.SetMiddlewareIPFilter(s.CreateShow)).Methods("POST")
-	s.Router.HandleFunc("/shows/{id}", middlewares.SetMiddlewareIPFilter(s.GetShow)).Methods("GET")
-	s.Router.HandleFunc("/shows/{id}", middlewares.SetMiddlewareIPFilter(s.UpdateShow)).Methods("PUT")
-	s.Router.HandleFunc("/shows/{id}", middlewares.SetMiddlewareIPFilter(s.DeleteShow)).Methods("DELETE")
-	s.Router.HandleFunc("/shows/{id}/producers", middlewares.SetMiddlewareIPFilter(s.GetShowProducers)).Methods("GET")
-	s.Router.HandleFunc("/shows/{id}/producers/{user_id}", middlewares.SetMiddlewareIPFilter(s.AddOrRemoveShowProducer)).Methods("PUT")
-	s.Router.HandleFunc("/shows/{id}/producers/{user_id}", middlewares.SetMiddlewareIPFilter(s.AddOrRemoveShowProducer)).Methods("DELETE")
-	s.Router.HandleFunc("/shows/{id}/urls", middlewares.SetMiddlewareIPFilter(s.GetShowUrls)).Methods("GET")
-	s.Router.HandleFunc("/shows/{id}/urls", middlewares.SetMiddlewareIPFilter(s.AddShowUrl)).Methods("POST")
-	s.Router.HandleFunc("/shows/{id}/urls", middlewares.SetMiddlewareIPFilter(s.RemoveShowUrl)).Methods("DELETE")
 
-	s.Router.HandleFunc("/shows/{id}/golive", middlewares.SetMiddlewareIPFilter(s.UpdateGoLive)).Methods("PUT")
-	s.Router.HandleFunc("/shows/{id}/activate", middlewares.SetMiddlewareIPFilter(s.SetActiveShow)).Methods("PUT")
-	s.Router.HandleFunc("/shows/{id}/deactivate", middlewares.SetMiddlewareIPFilter(s.SetActiveShow)).Methods("PUT")
+	s.Router.HandleFunc("/shows/{id:"+idRegEx+"}", middlewares.SetMiddlewareIPFilter(s.GetShow)).Methods("GET")
+	s.Router.HandleFunc("/shows/{id:"+idRegEx+"}", middlewares.SetMiddlewareIPFilter(s.UpdateShow)).Methods("PUT")
+	s.Router.HandleFunc("/shows/{id:"+idRegEx+"}", middlewares.SetMiddlewareIPFilter(s.DeleteShow)).Methods("DELETE")
+	s.Router.HandleFunc("/shows/{id:"+idRegEx+"}/producers", middlewares.SetMiddlewareIPFilter(s.GetShowProducers)).Methods("GET")
+	s.Router.HandleFunc("/shows/{id:"+idRegEx+"}/producers/{user_id}", middlewares.SetMiddlewareIPFilter(s.AddOrRemoveShowProducer)).Methods("PUT", "DELETE")
+	s.Router.HandleFunc("/shows/{id:"+idRegEx+"}/urls", middlewares.SetMiddlewareIPFilter(s.GetShowUrls)).Methods("GET")
+	s.Router.HandleFunc("/shows/{id:"+idRegEx+"}/urls", middlewares.SetMiddlewareIPFilter(s.AddShowUrl)).Methods("POST")
+	s.Router.HandleFunc("/shows/{id:"+idRegEx+"}/urls", middlewares.SetMiddlewareIPFilter(s.RemoveShowUrl)).Methods("DELETE")
 
-	s.Router.HandleFunc("/shows/{id}/messages", middlewares.SetMiddlewareIPFilter(s.GetMessages)).Methods("GET").Queries("page", "{page}").Name("Pagination")
-	s.Router.HandleFunc("/shows/{id}/messages", middlewares.SetMiddlewareIPFilter(s.GetMessages)).Methods("GET")
-	s.Router.HandleFunc("/shows/{id}/messages", middlewares.SetMiddlewareIPFilter(s.SendMessage)).Methods("POST")
+	s.Router.HandleFunc("/shows/{id:"+idRegEx+"}/golive", middlewares.SetMiddlewareIPFilter(s.UpdateGoLive)).Methods("PUT")
+	s.Router.HandleFunc("/shows/{id:"+idRegEx+"}/activate", middlewares.SetMiddlewareIPFilter(s.SetActiveShow)).Methods("PUT")
+	s.Router.HandleFunc("/shows/{id:"+idRegEx+"}/deactivate", middlewares.SetMiddlewareIPFilter(s.SetActiveShow)).Methods("PUT")
 
-	// Public
-
+	s.Router.HandleFunc("/shows/{id:"+idRegEx+"}/messages", middlewares.SetMiddlewareIPFilter(s.GetMessages)).Methods("GET").Queries("page", "{page:[1-9][0-9]*}").Name("Pagination")
+	s.Router.HandleFunc("/shows/{id:"+idRegEx+"}/messages", middlewares.SetMiddlewareIPFilter(s.GetMessages)).Methods("GET")
+	s.Router.HandleFunc("/shows/{id:"+idRegEx+"}/messages", middlewares.SetMiddlewareIPFilter(s.SendMessage)).Methods("POST")
 }
